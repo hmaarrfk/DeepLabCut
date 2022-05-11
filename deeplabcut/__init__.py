e7ae05269e4e72668d83c6c3177afff7361b6025
Mark Harfouche <mark.harfouche@gmail.com>
2022-05-11 07:08:09 -0400

    Avoid warning about not having pytorch installed.

--- deeplabcut/__init__.py
+++ deeplabcut/__init__.py
@@ -68,14 +68,8 @@ from deeplabcut.utils import (
 
 try:
     from deeplabcut.pose_tracking_pytorch import transformer_reID
-except ModuleNotFoundError as e:
-    import warnings
-    warnings.warn(
-        """
-        As PyTorch is not installed, unsupervised identity learning will not be available.
-        Please run `pip install torch`, or ignore this warning.
-        """
-    )
+except ModuleNotFoundError:
+    pass
 
 from deeplabcut.utils.auxfun_videos import (
     ShortenVideo,
